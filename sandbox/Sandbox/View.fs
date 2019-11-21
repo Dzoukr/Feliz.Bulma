482663e8cd9de3f021734e2584eb4af65b511f2b
Roman Provazník <dzoukr@dzoukr.cz>
2019-11-21 23:21:27 +0100

    QuickView extension kick-off

--- sandbox/Sandbox/View.fs
+++ sandbox/Sandbox/View.fs
@@ -5,10 +5,23 @@ open Fable.Core
 open Fable.React
 open Feliz
 open Feliz.Bulma
+open Feliz.Bulma.Extensions
     
 let view (model : Model) (dispatch : Msg -> unit) =
-    Bulma.section [
-        Bulma.message [
-            Bulma.messageBody "AHOJ"
+    QuickView.quickview [
+        //prop.className "is-active"
+        prop.children [
+            QuickView.quickviewHeader [
+                Html.div "AHOJ"
+            ]
+            QuickView.quickviewBody [
+                QuickView.quickviewBlock "AAA"
+                QuickView.quickviewBlock "BBB"
+            ]
+            QuickView.quickviewFooter [
+                Bulma.button "CLICK"
+            ]
         ]
     ]
+    
+    
