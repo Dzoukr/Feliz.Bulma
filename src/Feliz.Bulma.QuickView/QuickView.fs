a5ea2511cdf34a2682669d061eb2b16dff01ef94
Roman Provazník <dzoukr@dzoukr.cz>
2019-12-06 07:34:40 +0100

    Extract reused literals

--- src/Feliz.Bulma.QuickView/QuickView.fs
+++ src/Feliz.Bulma.QuickView/QuickView.fs
@@ -3,25 +3,32 @@
 open Feliz
 open Feliz.Bulma
 
+module private ElementLiterals =
+    let [<Literal>] ``quickview`` = "quickview"
+    let [<Literal>] ``quickview-header`` = "quickview-header"
+    let [<Literal>] ``quickview-body`` = "quickview-body"
+    let [<Literal>] ``quickview-block`` = "quickview-block"
+    let [<Literal>] ``quickview-footer`` = "quickview-footer"
+
 type QuickView =
-    static member inline quickview props = ElementBuilders.Div.props "quickview" props
-    static member inline quickview (elms:#seq<ReactElement>) = ElementBuilders.Div.children "quickview" elms
-    static member inline quickview elm = ElementBuilders.Div.valueElm "quickview" elm
+    static member inline quickview props = ElementBuilders.Div.props ElementLiterals.``quickview`` props
+    static member inline quickview (elms:#seq<ReactElement>) = ElementBuilders.Div.children ElementLiterals.``quickview`` elms
+    static member inline quickview elm = ElementBuilders.Div.valueElm ElementLiterals.``quickview`` elm
     
-    static member inline header props = ElementBuilders.Header.props "quickview-header" props
-    static member inline header (elms:#seq<ReactElement>) = ElementBuilders.Header.children "quickview-header" elms
-    static member inline header elm = ElementBuilders.Header.valueElm "quickview-header" elm
+    static member inline header props = ElementBuilders.Header.props ElementLiterals.``quickview-header`` props
+    static member inline header (elms:#seq<ReactElement>) = ElementBuilders.Header.children ElementLiterals.``quickview-header`` elms
+    static member inline header elm = ElementBuilders.Header.valueElm ElementLiterals.``quickview-header`` elm
     
-    static member inline body props = ElementBuilders.Div.props "quickview-body" props
-    static member inline body (elms:#seq<ReactElement>) = ElementBuilders.Div.children "quickview-body" elms
-    static member inline body elm = ElementBuilders.Div.valueElm "quickview-body" elm
+    static member inline body props = ElementBuilders.Div.props ElementLiterals.``quickview-body`` props
+    static member inline body (elms:#seq<ReactElement>) = ElementBuilders.Div.children ElementLiterals.``quickview-body`` elms
+    static member inline body elm = ElementBuilders.Div.valueElm ElementLiterals.``quickview-body`` elm
     
-    static member inline block props = ElementBuilders.Div.props "quickview-block" props
-    static member inline block (elms:#seq<ReactElement>) = ElementBuilders.Div.children "quickview-block" elms
-    static member inline block elm = ElementBuilders.Div.valueElm "quickview-block" elm
-    static member inline block s = ElementBuilders.Div.valueStr "quickview-block" s
-    static member inline block i = ElementBuilders.Div.valueInt "quickview-block" i
+    static member inline block props = ElementBuilders.Div.props ElementLiterals.``quickview-block`` props
+    static member inline block (elms:#seq<ReactElement>) = ElementBuilders.Div.children ElementLiterals.``quickview-block`` elms
+    static member inline block elm = ElementBuilders.Div.valueElm ElementLiterals.``quickview-block`` elm
+    static member inline block s = ElementBuilders.Div.valueStr ElementLiterals.``quickview-block`` s
+    static member inline block i = ElementBuilders.Div.valueInt ElementLiterals.``quickview-block`` i
     
-    static member inline footer props = ElementBuilders.Footer.props "quickview-footer" props
-    static member inline footer (elms:#seq<ReactElement>) = ElementBuilders.Footer.children "quickview-footer" elms
-    static member inline footer elm = ElementBuilders.Footer.valueElm "quickview-footer" elm
+    static member inline footer props = ElementBuilders.Footer.props ElementLiterals.``quickview-footer`` props
+    static member inline footer (elms:#seq<ReactElement>) = ElementBuilders.Footer.children ElementLiterals.``quickview-footer`` elms
+    static member inline footer elm = ElementBuilders.Footer.valueElm ElementLiterals.``quickview-footer`` elm
