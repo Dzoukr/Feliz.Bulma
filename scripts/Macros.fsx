3f3f0ca93748f97996a2c2aa4147eee1075d205d
Roman Provazník <dzoukr@dzoukr.cz>
2021-01-28 11:03:04 +0100

    Adding missing column responsiveness classes #19

--- scripts/Macros.fsx
+++ scripts/Macros.fsx
@@ -1,4 +1,4 @@
-﻿﻿let doType elm name className =
+﻿let doType elm name className =
     """
     static member inline {NAME} props = ElementBuilders.{ELM}.props "{CLASS}" props
     static member inline {NAME} (elms:#seq<ReactElement>) = ElementBuilders.{ELM}.children "{CLASS}" elms
@@ -32,7 +32,7 @@ let doIs list =
         let hasTextLeft{NAME} = PropertyBuilders.makeClass "has-text-left-{DEV}"
         let hasTextRight{NAME} = PropertyBuilders.makeClass "has-text-right-{DEV}"
         """
-        
+
         //"let isSize{NUM}FullHd = PropertyBuilders.mkClass \"is-size-{NUM}-fullhd\""
         |> fun x -> x.Replace("{NAME}", n).Replace("{DEV}",d)
         |> System.Console.WriteLine
@@ -46,10 +46,94 @@ let doIs list =
         "Widescreen", "widescreen"
         "WidescreenOnly", "widescreen-only"
         "FullHd", "fullhd"
-        
-        
+
+
     ] |> List.iter item
-doIs 7   
+doIs 7
+
+
+
+let ports = [
+    "",""
+    "Mobile", "mobile"
+    "Tablet", "tablet"
+    "TabletOnly", "tablet-only"
+    "Touch", "touch"
+    "Desktop", "desktop"
+    "DesktopOnly", "desktop-only"
+    "Widescreen", "widescreen"
+    "WidescreenOnly", "widescreen-only"
+    "FullHd", "fullhd"
+]
+
+let extendedValues = [
+    "is1", "is-1"
+    "is2", "is-2"
+    "is3", "is-3"
+    "is4", "is-4"
+    "is5", "is-5"
+    "is6", "is-6"
+    "is7", "is-7"
+    "is8", "is-8"
+    "is9", "is-9"
+    "is10", "is-10"
+    "is11", "is-11"
+    "is12", "is-12"
+]
+
+let values = [
+
+    "isThreeQuarters", "is-three-quarters"
+    "isTwoThirds", "is-two-thirds"
+    "isHalf", "is-half"
+    "isOneThird", "is-one-third"
+    "isOneQuarter", "is-one-quarter"
+    "isFull", "is-full"
+    "isFourFifths", "is-four-fifths"
+    "isThreeFifths", "is-three-fifths"
+    "isTwoFifths", "is-two-fifths"
+    "isOneFifth", "is-one-fifth"
+    "isNarrow", "is-narrow"
+
+    "isOffset1", "is-offset-1"
+    "isOffset2", "is-offset-2"
+    "isOffset3", "is-offset-3"
+    "isOffset4", "is-offset-4"
+    "isOffset5", "is-offset-5"
+    "isOffset6", "is-offset-6"
+    "isOffset7", "is-offset-7"
+    "isOffset8", "is-offset-8"
+    "isOffset9", "is-offset-9"
+    "isOffset10", "is-offset-10"
+    "isOffset11", "is-offset-11"
+    "isOffset12", "is-offset-12"
+
+    "isOffsetThreeQuarters", "is-offset-three-quarters"
+    "isOffsetTwoThirds", "is-offset-two-thirds"
+    "isOffsetHalf", "is-offset-half"
+    "isOffsetOneThird", "is-offset-one-third"
+    "isOffsetOneQuarter", "is-offset-one-quarter"
+    "isOffsetFull", "is-offset-full"
+    "isOffsetFourFifths", "is-offset-four-fifths"
+    "isOffsetThreeFifths", "is-offset-three-fifths"
+    "isOffsetTwoFifths", "is-offset-two-fifths"
+    "isOffsetOneFifth", "is-offset-one-fifth"
+
+]
+
+[
+    for vn,vc in extendedValues do
+        for pn,pc in ports do
+            let nc = if pc.Length > 0 then $"{vc}-{pc}" else vc
+            //$"static member inline {vn+pn} = PropertyBuilders.mkClass ClassLiterals.``{nc}``"
+            $"let [<Literal>] ``{nc}`` = \"{nc}\""
+]
+|> (fun lines -> System.IO.File.WriteAllLines("c:\\temp\\newbulma.txt", lines))
+
+
+
+
+
 
 let doProps ports v  =
     let vals = [
@@ -61,7 +145,7 @@ let doProps ports v  =
                     .Replace("{PO}",po)
                     .Replace("{PU}",pu)
     ]
-    
+
     vals
     |> String.concat "\n"
     |> System.Console.WriteLine
